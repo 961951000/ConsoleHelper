00f1d1c450649a5e028fd94e786a5513ec474ee9
Xiangwei Cai <v-caxian@microsoft.com>
2017-12-25 15:57:42 +0800

    add GitHubManager

---
+++ ConsoleHelper/Helpers/GitHubManager.cs
@@ -0,0 +1,32 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHelper.Helpers
+{
+    public class GitHubManager
+    {
+        private readonly IGitHubClient _githubClient;
+        public GitHubManager()
+        {
+            _githubClient = _githubClient ?? new GitHubClient(new ProductHeaderValue("ApplicationName"));
+        }
+        public async Task<IEnumerable<User>> GetUserListAsync(string gitHubTokens)
+        {
+            var tokens = gitHubTokens.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var tasks = tokens.Select(GetCurrentUserAsync);
+
+            return await Task.WhenAll(tasks);
+        }
+
+        private async Task<User> GetCurrentUserAsync(string token)
+        {
+            _githubClient.Connection.Credentials = new Credentials(token);
+
+            return await _githubClient.User.Current();
+        }
+    }
+}
--- ConsoleHelper/Program.cs
+++ ConsoleHelper/Program.cs
@@ -1,16 +1,6 @@
-﻿using AutoMapper;
-using ConsoleHelper.Models;
-using ConsoleHelper.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
+﻿using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CsvHelper;
 using ConsoleHelper.Helpers;
-using System.Diagnostics;
 
 namespace ConsoleHelper
 {
@@ -18,15 +8,7 @@ namespace ConsoleHelper
     {
         static void Main(string[] args)
         {
-            var watch = Stopwatch.StartNew();
-            var result = FileHelper.GetAllDirectory(new DirectoryInfo(@"D:\Repos\MicrosoftDocs\docs")).Result.ToList();
-            watch.Stop();
-            Console.WriteLine(watch.ElapsedMilliseconds);
-            watch.Reset();
-            watch.Restart();
-            var result1 = FileHelper.GetAllFiles(new DirectoryInfo(@"D:\Repos\MicrosoftDocs\docs")).Result.ToList();
-            Console.WriteLine(watch.ElapsedMilliseconds);
-            watch.Stop();
+
             Console.WriteLine("End...");
             Console.Read();
         }
